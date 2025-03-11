62904f869f760f2bb504a181ed7d669145d06e94
amao03 <alice.mao818@gmail.com>
2025-03-11 18:52:49 -0400

    added made by me statement

--- alice-website/src/Contact.js
+++ alice-website/src/Contact.js
@@ -6,24 +6,28 @@ import { MdOutlineEmail } from "react-icons/md";
 
 function Contact() {
     return (
-        <div id="contact_main">
-            <div class="box">
-                <h1 class="Contact_title">Contact</h1>
+        <div id="contact_bigbox">
+            <div id="contact_main">
+                <div class="box">
+                    <h1 class="Contact_title">Contact</h1>
+                </div>
+                <div class="box">
+                    <div class="contact">
+                        <h3 class="contact_type">Email</h3>
+                        <p class="contact_info"><MdOutlineEmail /> <a href="mailto:alice.mao818@gamil.com">alice.mao818@gamil.com</a></p>
+                    </div>
+                    <div class="contact">
+                        <h3 class="contact_type">LinkedIn</h3>
+                        <p class="contact_info"><FaLinkedin /> <a href="https://linkedin.com/in/alicehmao">Connect with me</a></p>
+                    </div>
+                    <div class="contact">
+                        <h3 class="contact_type">Github</h3>
+                        <p class="contact_info" id="last_contact"><FaGithub /> <a href="https://github.com/alicemao03/projects/">My projects</a></p>
+                    </div>
+                </div>
+
             </div>
-            <div class="box">
-                <div class="contact">
-                    <h3 class="contact_type">Email</h3>
-                    <p class="contact_info"><MdOutlineEmail /> <a href="mailto:alice.mao818@gamil.com">alice.mao818@gamil.com</a></p>
-                </div>
-                <div class="contact">
-                    <h3 class="contact_type">LinkedIn</h3>
-                    <p class="contact_info"><FaLinkedin /> <a href="https://linkedin.com/in/alicehmao">Connect with me</a></p>
-                </div>
-                <div class="contact">
-                    <h3 class="contact_type">Github</h3>
-                    <p class="contact_info"><FaGithub /> <a href="https://github.com/alicemao03/projects/">My projects</a></p>
-                </div>
-            </div>
+            <p class="copyright">This website was made by me, Alice Mao!</p>
         </div>
     );
 }
